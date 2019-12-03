4b6338fe2b4874978369763cd9c27f1b809b5a2c
Sebastian Moksnes <sebastian@moksnes.se>
2019-12-03 08:42:21 +0100

    Add project files.

---
+++ Moksnes.BuyStuff.GrainInterfaces/IHello.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Moksnes.BuyStuff.GrainInterfaces
+{
+    public interface IHello : Orleans.IGrainWithIntegerKey
+    {
+        Task<string> SayHello(string greeting);
+    }
+}
---
+++ Moksnes.BuyStuff.Grains/HelloGrain.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moksnes.BuyStuff.GrainInterfaces;
+
+namespace Moksnes.BuyStuff.Grains
+{
+    public class HelloGrain : Orleans.Grain, IHello
+    {
+        private readonly ILogger logger;
+
+        public HelloGrain(ILogger<HelloGrain> logger)
+        {
+            this.logger = logger;
+        }
+
+        Task<string> IHello.SayHello(string greeting)
+        {
+            logger.LogInformation($"\n SayHello message received: greeting = '{greeting}'");
+            return Task.FromResult($"\n Client said: '{greeting}', so HelloGrain says: Hello!");
+        }
+    }
+}
